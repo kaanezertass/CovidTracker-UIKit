7c2d18dd75824390117d65bb0bd3b04ad9d2db0a
Elka Software <48525469+elkasoftwaree@users.noreply.github.com>
2023-12-23 17:31:37 +0300

    Initial Commit

---
+++ CovidApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CovidApp
+//
+//  Created by Kaan Ezerrtaş on 23.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
